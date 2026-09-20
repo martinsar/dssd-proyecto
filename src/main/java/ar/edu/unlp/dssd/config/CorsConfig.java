cc40fb2ad0f6ee4efff8978ef9be975a8371401b
martin <martinsarachaga@abc.gob.ar>
2026-09-20 19:50:35 -0300

    agregado Swagger

---
+++ src/main/java/ar/edu/unlp/dssd/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package ar.edu.unlp.dssd.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todos los endpoints de tu API
+                .allowedOrigins("http://localhost:4200") // Permite el puerto de tu Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
